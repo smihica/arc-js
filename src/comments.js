943124e9a95599856a420a8483230cf60bc376d7
smihica <smihica@gmail.com>
2014-06-03 10:31:59 +0900

    Fixed comments.

--- src/comments.js
+++ src/comments.js
@@ -1,6 +1,6 @@
 /*
  Arc-JS --- The Arc-Language's compiler, stack VM-interpreter written in Javascript.
  -----------------------------------------------------------------------------------
- License:  MIT License, Copyright (C) 2013 Shin Aoyama (@smihica)
+ License:  Perl Foundations's Artistic License 2.0, Copyright (C) 2013 Shin Aoyama (@smihica)
  Site:     https://github.com/smihica/arc-js
 */
